0f50d7d0e6dd330107132802b34331c5f239dcc1
soramameen <sorananonaomasa.sora@gmail.com>
2024-12-25 14:32:02 +0900

    herokuのデプロイ用にポート番号を変更

--- main.go
+++ main.go
@@ -4,49 +4,56 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
 
 // WebSocketのアップグレード用
 var upgrader = websocket.Upgrader{
-    CheckOrigin: func(r *http.Request) bool {
-        return true // 開発中のCORSを許可
-    },
+	CheckOrigin: func(r *http.Request) bool {
+		return true // 開発中のCORSを許可
+	},
 }
 
 // WebSocketハンドラー
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-    // HTTP接続をWebSocketにアップグレード
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println("アップグレードエラー:", err)
-        return
-    }
-    defer conn.Close()
+	// HTTP接続をWebSocketにアップグレード
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("アップグレードエラー:", err)
+		return
+	}
+	defer conn.Close()
 
-    log.Println("クライアント接続成功")
+	log.Println("クライアント接続成功")
 
-    for {
-        // クライアントからのメッセージを受信
-        _, msg, err := conn.ReadMessage()
-        if err != nil {
-            log.Println("受信エラー:", err)
-            break
-        }
-        log.Printf("受信メッセージ: %s\n", msg)
+	for {
+		// クライアントからのメッセージを受信
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("受信エラー:", err)
+			break
+		}
+		log.Printf("受信メッセージ: %s\n", msg)
 
-        // メッセージをそのまま送り返す
-        if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-            log.Println("送信エラー:", err)
-            break
-        }
-    }
+		// メッセージをそのまま送り返す
+		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			log.Println("送信エラー:", err)
+			break
+		}
+	}
 }
 
 func main() {
-    http.HandleFunc("/ws", handleWebSocket)
+	// ポート番号を環境変数から取得
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // デフォルトポート番号
+	}
 
-    fmt.Println("WebSocketサーバー起動: http://localhost:8080/ws")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/ws", handleWebSocket)
+
+	fmt.Printf("WebSocketサーバー起動: http://localhost:%s/ws\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
