426a093c0c73c3235f2f458e08cbf37eebd277c1
soramameen <sorananonaomasa.sora@gmail.com>
2024-12-25 15:02:18 +0900

    chatの言う通り

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -11,94 +12,50 @@ import (
 // WebSocketのアップグレード用
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return true // 開発中のCORSを許可
+		return true // 必要ならCORSを許可
 	},
 }
 
-// クライアントを表す構造体
-type Client struct {
-	conn *websocket.Conn
-	send chan []byte
-}
-
-// 接続されている全クライアントを管理
-var clients = make(map[*Client]bool)
-
-// メッセージをブロードキャストするためのチャネル
-var broadcast = make(chan []byte)
-
-func main() {
-	// メッセージを処理するゴルーチン
-	go handleMessages()
-
-	// WebSocketエンドポイントを定義
-	http.HandleFunc("/ws", handleWebSocket)
-
-	fmt.Println("WebSocketサーバー起動: http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-// WebSocket接続を処理
+// WebSocketハンドラー
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	// HTTP接続をWebSocketにアップグレード
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("アップグレードエラー:", err)
+		log.Println("WebSocketアップグレードエラー:", err)
 		return
 	}
 	defer conn.Close()
 
-	// 新しいクライアントを作成
-	client := &Client{
-		conn: conn,
-		send: make(chan []byte),
-	}
+	log.Println("クライアント接続成功")
 
-	// クライアントを登録
-	clients[client] = true
-
-	// クライアントの受信・送信処理を並行して実行
-	go client.readPump()
-	go client.writePump()
-
-	// クライアントが切断されたら削除
-	defer func() {
-		delete(clients, client)
-		close(client.send)
-	}()
-}
-
-// クライアントのメッセージ受信処理
-func (c *Client) readPump() {
+	// クライアントからのメッセージを受信してそのまま返す
 	for {
-		_, message, err := c.conn.ReadMessage()
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
-			log.Println("メッセージ受信エラー:", err)
+			log.Println("受信エラー:", err)
 			break
 		}
-		// 受信したメッセージをブロードキャストチャネルへ送る
-		broadcast <- message
-	}
-}
+		log.Printf("受信メッセージ: %s\n", msg)
 
-// クライアントのメッセージ送信処理
-func (c *Client) writePump() {
-	for message := range c.send {
-		if err := c.conn.WriteMessage(websocket.TextMessage, message); err != nil {
-			log.Println("メッセージ送信エラー:", err)
+		// メッセージをクライアントに返す
+		err = conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Println("送信エラー:", err)
 			break
 		}
 	}
 }
 
-// メッセージを全クライアントにブロードキャスト
-func handleMessages() {
-	for {
-		// broadcastチャネルからメッセージを受け取る
-		message := <-broadcast
-		// すべてのクライアントに送信
-		for client := range clients {
-			client.send <- message
-		}
+func main() {
+	// 環境変数 PORT を取得
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set") // Herokuでは$PORTが必要
 	}
+
+	http.HandleFunc("/ws", handleWebSocket) // WebSocketエンドポイント
+
+	// サーバー起動
+	fmt.Printf("WebSocketサーバー起動: http://localhost:%s/ws\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
